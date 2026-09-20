609e19962bcabafdd7cc318e60cf7aafb19d42cd
WorldWideAirlinesTicketsBooking <worldwideairlinesticketsbookin@gmail.com>
2026-09-20 16:11:27 +0500

    Update booking confirmation message in script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("bookingForm").addEventListener("submit", function(e){
     e.preventDefault();
 
     document.getElementById("message").innerHTML =
-        "Thank you! Your inquiry has been submitted successfully.";
+    "Thank you! Your booking inquiry has been received. We will contact you shortly.";
 
     this.reset();
 });
